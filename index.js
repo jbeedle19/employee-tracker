4fc296e28f51322dcaaf2b74d2217a6c8a3f2b99
jbeedle19 <josh.beedle@gmail.com>
2021-01-22 13:45:28 -0500

    added comments to outline what the project needs

---
+++ index.js
@@ -0,0 +1,18 @@
+// Prompt Choices after starting app
+// View: 
+//      All Departments
+//          show formatted table with department names and department ids
+//      All Roles
+//          show formatted table with job title, role id, the department that role belongs to, and the salary for that role
+//      All Employees 
+//          show formatted table with employee ids, first names, last names, job titles, departments, salaries, and manager of that employee
+// Add:
+//      Department
+//          prompted to enter department name. Department is added to DB
+//      Role
+//          prompted to enter the name, salary, and department for the role. Role is added to DB
+//      Employee
+//          prompted to enter the employee's first name, last name, role, and manager. Employee is added to DB
+// Update:
+//      Employee's role
+//          prompted to select an employee to update and their role and entered info is updated in the DB
